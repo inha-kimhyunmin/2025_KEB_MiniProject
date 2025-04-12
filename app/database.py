d39872ffeab9b7f71a1db79b259dc9a9416d23d4
inha-kimhyunmin <kimhm396@gmail.com>
2025-04-12 22:40:16 +0900

    V0.11 error 해결

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://user:password@localhost:3306/yourdatabase"
+DATABASE_URL = "mysql+pymysql://root:worldcup7!@localhost:3306/yourdatabase"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from .database import Base
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255))
     payments = relationship("Payment", back_populates="user")
 
 class Payment(Base):
@@ -15,5 +15,5 @@ class Payment(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     amount = Column(Integer)
     date = Column(DateTime)
-    merchant_name = Column(String)
+    merchant_name = Column(String(255))
     user = relationship("User", back_populates="payments")
