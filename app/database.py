96a8c0900eb77bfaafdf37b98a43c380fa5e0f25
inha-kimhyunmin <kimhm396@gmail.com>
2025-04-12 22:26:44 +0900

    V0.1 database with Mysql

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./mydata.db"
+DATABASE_URL = "mysql+pymysql://user:password@localhost:3306/yourdatabase"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
